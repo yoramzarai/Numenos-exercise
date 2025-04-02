eeaa04f147427350de454e48a16c5086633727fd
yoramzarai <yoram.zarai@gmail.com>
2025-04-02 15:38:45 +0300

    added sratoolkit class implementation

---
+++ mytrust_example1_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# we use instead docker and run it in the notebook
+#./run-trust4 -f hg38_bcrtcr.fa --ref human_IMGT+C.fa -b example/example.bam -o example_test
+
+if [ ! -e "example/Output/TRUST_example_1_report.tsv" ]; then
+	echo "Failed to run TRUST4. Please check whether TRUST4 is compiled with command 'make' or the other files are in the right paths."	
+	exit
+fi
+
+if [[ $(diff <(sort example/Output/TRUST_example_1_report.tsv) <(sort example/TRUST_example_report.tsv) | wc -l) -ge 1 ]]; then
+	echo "Results do not match the pregenerated example results. Please check TRUST4's version or the example files are in the folder."	
+	exit
+fi
+
+#rm example_test_*
+
+echo "TRUST4 is ready to use."
